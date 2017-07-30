3f4b2f4a4d376fc364dfe1e3d1ff287b1f5a361b
Arslan Arshad <aac.1989@hotmail.com>
2017-07-30 15:50:58 +0900

    Geofencing Added

--- app/src/main/java/com/example/android/shushme/Geofencing.java
+++ app/src/main/java/com/example/android/shushme/Geofencing.java
@@ -24,121 +24,132 @@ import java.util.List;
 
 public class Geofencing implements ResultCallback {
 
-    private static final long GEOFENCE_TIMEOUT = 86400000;
-    private static final float GEOFENCE_RADIUS = 100000 ;
-    private static final String TAG = Geofencing.class.getSimpleName() ;
+    // Constants
+    public static final String TAG = Geofencing.class.getSimpleName();
+    private static final float GEOFENCE_RADIUS = 50000;
+    private static final long GEOFENCE_TIMEOUT = 24 * 60 * 60 * 1000; // 24 hours
 
-    GoogleApiClient mGoogleApiClient;
-    Context mContext;
-    PendingIntent mGeofencePendingIntent;
-    List<Geofence> mGeofenceList ;
+    private List<Geofence> mGeofenceList;
+    private PendingIntent mGeofencePendingIntent;
+    private GoogleApiClient mGoogleApiClient;
+    private Context mContext;
 
-    public Geofencing (Context context, GoogleApiClient googleApiClient){
-        this.mContext = context;
-        this.mGoogleApiClient = googleApiClient;
+    public Geofencing(Context context, GoogleApiClient client) {
+        mContext = context;
+        mGoogleApiClient = client;
         mGeofencePendingIntent = null;
         mGeofenceList = new ArrayList<>();
-
     }
 
     /***
-     * Registers the list of geofences specified in mGeofenceList
-     * Uses mGoogleApiClient to connect to the Google API Client
-     * Uses getGeofencingRequest to get the list of geofences to be registered
-     * Uses getGeofencePendingIntent to get the pending Intent to launch the PendingIntent
+     * Registers the list of Geofences specified in mGeofenceList with Google Place Services
+     * Uses {@code #mGoogleApiClient} to connect to Google Place Services
+     * Uses {@link #getGeofencingRequest} to get the list of Geofences to be registered
+     * Uses {@link #getGeofencePendingIntent} to get the pending intent to launch the IntentService
+     * when the Geofence is triggered
+     * Triggers {@link #onResult} when the geofences have been registered successfully
      */
-    public void registerAllGeofences (){
-        if (mGoogleApiClient==null || !mGoogleApiClient.isConnected()
-            || mGeofenceList == null || mGeofenceList.size()==0){
-
+    public void registerAllGeofences() {
+        // Check that the API client is connected and that the list has Geofences in it
+        if (mGoogleApiClient == null || !mGoogleApiClient.isConnected() ||
+                mGeofenceList == null || mGeofenceList.size() == 0) {
+            return;
         }
         try {
             LocationServices.GeofencingApi.addGeofences(
-              mGoogleApiClient,
+                    mGoogleApiClient,
                     getGeofencingRequest(),
                     getGeofencePendingIntent()
             ).setResultCallback(this);
-        } catch (SecurityException securityException){
-            Log.e(TAG,"Exception : " + securityException.getMessage());
+        } catch (SecurityException securityException) {
+            // Catch exception generated if the app does not use ACCESS_FINE_LOCATION permission.
+            Log.e(TAG, securityException.getMessage());
         }
-
     }
 
     /***
-     * Registers the list of geofences specified in mGeofenceList
-     * Uses mGoogleApiClient to connect to the Google API Client
-     * Uses getGeofencePendingIntent to get the pending Intent to launch the PendingIntent
+     * Unregisters all the Geofences created by this app from Google Place Services
+     * Uses {@code #mGoogleApiClient} to connect to Google Place Services
+     * Uses {@link #getGeofencePendingIntent} to get the pending intent passed when
+     * registering the Geofences in the first place
+     * Triggers {@link #onResult} when the geofences have been unregistered successfully
      */
-    public void unRegisterAllGeofences(){
-        if (mGoogleApiClient==null || !mGoogleApiClient.isConnected()){
-        return;
+    public void unRegisterAllGeofences() {
+        if (mGoogleApiClient == null || !mGoogleApiClient.isConnected()) {
+            return;
         }
         try {
             LocationServices.GeofencingApi.removeGeofences(
                     mGoogleApiClient,
+                    // This is the same pending intent that was used in registerGeofences
                     getGeofencePendingIntent()
             ).setResultCallback(this);
-        } catch (SecurityException securityException){
-            Log.e(TAG,"Exception : " + securityException.getMessage());
+        } catch (SecurityException securityException) {
+            // Catch exception generated if the app does not use ACCESS_FINE_LOCATION permission.
+            Log.e(TAG, securityException.getMessage());
         }
-
     }
 
 
     /***
-     * Updates the local ArrayList of Geofences with data passed in the list.
+     * Updates the local ArrayList of Geofences using data from the passed in list
+     * Uses the Place ID defined by the API as the Geofence object Id
      *
-     * @param places is the PlaceBuffer result of getPlaceById method.
+     * @param places the PlaceBuffer result of the getPlaceById call
      */
-    public void updateGeofenceList(PlaceBuffer places){
+    public void updateGeofencesList(PlaceBuffer places) {
         mGeofenceList = new ArrayList<>();
-        if (places == null || places.getCount()==0) return;
-        for(Place place : places){
+        if (places == null || places.getCount() == 0) return;
+        for (Place place : places) {
+            // Read the place information from the DB cursor
             String placeUID = place.getId();
-            double placeLatitude = place.getLatLng().latitude;
-            double placeLongitude = place.getLatLng().longitude;
-            // build a geofence object
+            double placeLat = place.getLatLng().latitude;
+            double placeLng = place.getLatLng().longitude;
+            // Build a Geofence object
             Geofence geofence = new Geofence.Builder()
                     .setRequestId(placeUID)
                     .setExpirationDuration(GEOFENCE_TIMEOUT)
-                    .setCircularRegion(placeLatitude,placeLongitude,GEOFENCE_RADIUS)
+                    .setCircularRegion(placeLat, placeLng, GEOFENCE_RADIUS)
                     .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER | Geofence.GEOFENCE_TRANSITION_EXIT)
                     .build();
+            // Add it to the list
             mGeofenceList.add(geofence);
         }
     }
 
     /***
-     * Creates a GeofencingRequest object using mGeofenceList ArrayList
+     * Creates a GeofencingRequest object using the mGeofenceList ArrayList of Geofences
+     * Used by {@code #registerGeofences}
      *
-     * @return
+     * @return the GeofencingRequest object
      */
-    private GeofencingRequest getGeofencingRequest(){
+    private GeofencingRequest getGeofencingRequest() {
         GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
-        // setInitialTrigger tells what will happen if the device is already in Geofence we are about to register
-        // INITIAL_TRIGGER_ENTER triger Entry Transition Event immediately
         builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
         builder.addGeofences(mGeofenceList);
         return builder.build();
     }
 
     /***
-     * Creates a PendingIntent Object using GeofenceTransitionsIntentService class
+     * Creates a PendingIntent object using the GeofenceTransitionsIntentService class
+     * Used by {@code #registerGeofences}
      *
-     * @return PendingIntent Object
+     * @return the PendingIntent object
      */
-    private PendingIntent getGeofencePendingIntent(){
-        // Reuse the pending Intent if we already have
-        if (mGeofencePendingIntent!=null){
+    private PendingIntent getGeofencePendingIntent() {
+        // Reuse the PendingIntent if we already have it.
+        if (mGeofencePendingIntent != null) {
             return mGeofencePendingIntent;
         }
         Intent intent = new Intent(mContext, GeofenceBroadcastReceiver.class);
-        mGeofencePendingIntent = PendingIntent.getBroadcast(mContext,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+        mGeofencePendingIntent = PendingIntent.getBroadcast(mContext, 0, intent, PendingIntent.
+                FLAG_UPDATE_CURRENT);
         return mGeofencePendingIntent;
     }
 
     @Override
     public void onResult(@NonNull Result result) {
-        Log.e(TAG, "Error adding/removing Geofences : %s " + result.getStatus().toString());
+        Log.e(TAG, String.format("Error adding/removing geofence : %s",
+                result.getStatus().toString()));
     }
 }
--- app/src/main/java/com/example/android/shushme/MainActivity.java
+++ app/src/main/java/com/example/android/shushme/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.shushme;
 
 import android.Manifest;
+import android.app.NotificationManager;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -38,20 +39,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
-
-    CheckBox locationsPermissions;
-    private static final int PERMISSIONS_REQUEST_FINE_LOCATION = 111;
-    public static final int PLACE_PICKER_REQUEST = 1;
     // Constants
     public static final String TAG = MainActivity.class.getSimpleName();
+    private static final int PERMISSIONS_REQUEST_FINE_LOCATION = 111;
+    private static final int PLACE_PICKER_REQUEST = 1;
 
     // Member variables
     private PlaceListAdapter mAdapter;
     private RecyclerView mRecyclerView;
-    private GoogleApiClient mClient;
-
-    Geofencing mGeoFencing;
     private boolean mIsEnabled;
+    private GoogleApiClient mClient;
+    private Geofencing mGeofencing;
 
     /**
      * Called when the activity is starting
@@ -63,32 +61,32 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        locationsPermissions = (CheckBox) findViewById(R.id.location_permission_checkbox);
         // Set up the recycler view
         mRecyclerView = (RecyclerView) findViewById(R.id.places_list_recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mAdapter = new PlaceListAdapter(this,null);
+        mAdapter = new PlaceListAdapter(this, null);
         mRecyclerView.setAdapter(mAdapter);
 
-        // Initialize the switch state and handle enable / disable switch change
-        Switch offSwitch = (Switch) findViewById(R.id.enable_switch);
-        mIsEnabled = getPreferences(MODE_PRIVATE).getBoolean(getString(R.string.setting_enabled),false);
-        offSwitch.setChecked(mIsEnabled);
-        offSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        // Initialize the switch state and Handle enable/disable switch change
+        Switch onOffSwitch = (Switch) findViewById(R.id.enable_switch);
+        mIsEnabled = getPreferences(MODE_PRIVATE).getBoolean(getString(R.string.setting_enabled), false);
+        onOffSwitch.setChecked(mIsEnabled);
+        onOffSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 SharedPreferences.Editor editor = getPreferences(MODE_PRIVATE).edit();
                 editor.putBoolean(getString(R.string.setting_enabled), isChecked);
                 mIsEnabled = isChecked;
                 editor.commit();
-                if (isChecked) {
-                    mGeoFencing.registerAllGeofences();
-                } else {
-                    mGeoFencing.unRegisterAllGeofences();
-                }
+                if (isChecked) mGeofencing.registerAllGeofences();
+                else mGeofencing.unRegisterAllGeofences();
             }
+
         });
 
+        // Build up the LocationServices API client
+        // Uses the addApi method to request the LocationServices API
+        // Also uses enableAutoManage to automatically when to connect/suspend the client
         mClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
@@ -97,43 +95,42 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                 .enableAutoManage(this, this)
                 .build();
 
-        mGeoFencing = new Geofencing(this,mClient);
+        mGeofencing = new Geofencing(this, mClient);
 
     }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (ActivityCompat.checkSelfPermission(MainActivity.this,
-                Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
-            locationsPermissions.setChecked(false);
-        } else {
-            locationsPermissions.setEnabled(true);
-            locationsPermissions.setChecked(true);
-        }
-    }
 
-
+    /***
+     * Called when the Google API Client is successfully connected
+     *
+     * @param connectionHint Bundle of data provided to clients by Google Play services
+     */
     @Override
-    public void onConnected(@Nullable Bundle bundle) {
+    public void onConnected(@Nullable Bundle connectionHint) {
         refreshPlacesData();
+        Log.i(TAG, "API Client Connection Successful!");
     }
 
+    /***
+     * Called when the Google API Client is suspended
+     *
+     * @param cause cause The reason for the disconnection. Defined by constants CAUSE_*.
+     */
     @Override
-    public void onConnectionSuspended(int i) {
-
+    public void onConnectionSuspended(int cause) {
+        Log.i(TAG, "API Client Connection Suspended!");
     }
 
+    /***
+     * Called when the Google API Client failed to connect to Google Play Services
+     *
+     * @param result A ConnectionResult that can be used for resolving the error
+     */
     @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-    public void onLocationPermissionClicked(View view) {
-            ActivityCompat.requestPermissions(MainActivity.this,
-                new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                                PERMISSIONS_REQUEST_FINE_LOCATION);
+    public void onConnectionFailed(@NonNull ConnectionResult result) {
+        Log.e(TAG, "API Client Connection Failed!");
     }
 
-    public void refreshPlacesData(){
+    public void refreshPlacesData() {
         Uri uri = PlaceContract.PlaceEntry.CONTENT_URI;
         Cursor data = getContentResolver().query(
                 uri,
@@ -141,37 +138,66 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                 null,
                 null,
                 null);
-        if (data == null || data.getCount()==0) return;
-        List<String> guids = new ArrayList<>();
-        while (data.moveToNext()){
+
+        if (data == null || data.getCount() == 0) return;
+        List<String> guids = new ArrayList<String>();
+        while (data.moveToNext()) {
             guids.add(data.getString(data.getColumnIndex(PlaceContract.PlaceEntry.COLUMN_PLACE_ID)));
         }
-        // PlaceBuffer is Places API object which access the list of places
-        PendingResult<PlaceBuffer> placesResult = Places.GeoDataApi.getPlaceById(mClient,
+        PendingResult<PlaceBuffer> placeResult = Places.GeoDataApi.getPlaceById(mClient,
                 guids.toArray(new String[guids.size()]));
-        placesResult.setResultCallback(new ResultCallback<PlaceBuffer>() {
+        placeResult.setResultCallback(new ResultCallback<PlaceBuffer>() {
             @Override
             public void onResult(@NonNull PlaceBuffer places) {
                 mAdapter.swapPlaces(places);
-                mGeoFencing.updateGeofenceList(places);
-                if (mIsEnabled){
-                    mGeoFencing.registerAllGeofences();
-                }
+                mGeofencing.updateGeofencesList(places);
+                if (mIsEnabled) mGeofencing.registerAllGeofences();
             }
         });
     }
 
-    @Override
+    /***
+     * Button Click event handler to handle clicking the "Add new location" Button
+     *
+     * @param view
+     */
+    public void onAddPlaceButtonClicked(View view) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, getString(R.string.need_location_permission_message), Toast.LENGTH_LONG).show();
+            return;
+        }
+        try {
+            // Start a new Activity for the Place Picker API, this will trigger {@code #onActivityResult}
+            // when a place is selected or with the user cancels.
+            PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+            Intent i = builder.build(this);
+            startActivityForResult(i, PLACE_PICKER_REQUEST);
+        } catch (GooglePlayServicesRepairableException e) {
+            Log.e(TAG, String.format("GooglePlayServices Not Available [%s]", e.getMessage()));
+        } catch (GooglePlayServicesNotAvailableException e) {
+            Log.e(TAG, String.format("GooglePlayServices Not Available [%s]", e.getMessage()));
+        } catch (Exception e) {
+            Log.e(TAG, String.format("PlacePicker Exception: %s", e.getMessage()));
+        }
+    }
+
+
+    /***
+     * Called when the Place Picker Activity returns back with a selected place (or after canceling)
+     *
+     * @param requestCode The request code passed when calling startActivityForResult
+     * @param resultCode  The result code specified by the second activity
+     * @param data        The Intent that carries the result data.
+     */
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        if (requestCode == PLACE_PICKER_REQUEST && resultCode == RESULT_OK){
-
-            Place place = PlacePicker.getPlace(this,data);
-
+        if (requestCode == PLACE_PICKER_REQUEST && resultCode == RESULT_OK) {
+            Place place = PlacePicker.getPlace(this, data);
             if (place == null) {
                 Log.i(TAG, "No place selected");
                 return;
             }
+
             // Extract the place information from the API
             String placeName = place.getName().toString();
             String placeAddress = place.getAddress().toString();
@@ -181,30 +207,46 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
             ContentValues contentValues = new ContentValues();
             contentValues.put(PlaceContract.PlaceEntry.COLUMN_PLACE_ID, placeID);
             getContentResolver().insert(PlaceContract.PlaceEntry.CONTENT_URI, contentValues);
+
+            // Get live data information
             refreshPlacesData();
-            Toast.makeText(this,  placeName + " was selected and inserted" , Toast.LENGTH_LONG).show();
         }
     }
 
-    public void onAddPlaceButtonClicked(View view) {
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(this, getString(R.string.need_location_permission_message), Toast.LENGTH_LONG).show();
-            return;
-        }
-        Toast.makeText(this, getString(R.string.location_permissions_granted_message), Toast.LENGTH_LONG).show();
+    @Override
+    public void onResume() {
+        super.onResume();
 
-        try {
-            PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
-            Intent i = builder.build(this);
-            startActivityForResult(i,PLACE_PICKER_REQUEST);
-        } catch (GooglePlayServicesRepairableException e) {
-            Log.e(TAG, String.format("GooglePlayServices Not Available [%s]", e.getMessage()));
-        } catch (GooglePlayServicesNotAvailableException e) {
-            Log.e(TAG, String.format("GooglePlayServices Not Available [%s]", e.getMessage()));
-        } catch (Exception e) {
-            Log.e(TAG, String.format("PlacePicker Exception: %s", e.getMessage()));
+        // Initialize location permissions checkbox
+        CheckBox locationPermissions = (CheckBox) findViewById(R.id.location_permission_checkbox);
+        if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            locationPermissions.setChecked(false);
+        } else {
+            locationPermissions.setChecked(true);
+            locationPermissions.setEnabled(false);
         }
 
+        // Initialize ringer permissions checkbox
+        CheckBox ringerPermissions = (CheckBox) findViewById(R.id.ringer_permissions_checkbox);
+        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        // Check if the API supports such permission change and check if permission is granted
+        if (android.os.Build.VERSION.SDK_INT >= 24 && !nm.isNotificationPolicyAccessGranted()) {
+            ringerPermissions.setChecked(false);
+        } else {
+            ringerPermissions.setChecked(true);
+            ringerPermissions.setEnabled(false);
+        }
+    }
+
+    public void onRingerPermissionsClicked(View view) {
+        Intent intent = new Intent(android.provider.Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+        startActivity(intent);
+    }
+
+    public void onLocationPermissionClicked(View view) {
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                PERMISSIONS_REQUEST_FINE_LOCATION);
     }
 }
